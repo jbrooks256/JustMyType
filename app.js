c018ced939d895d52ef28c4496fd672ecd9e33fb
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-28 12:18:36 -0500

    added working shift key

--- app.js
+++ app.js
@@ -2,4 +2,18 @@ $(document).ready(function () {
     
     $("#keyboard-upper-container").hide();
 
+    $(document).keydown(function (e) {
+        if (e.keyCode = 16) {
+            $("#keyboard-lower-container").hide();
+            $("#keyboard-upper-container").show();
+        }; 
+    });
+    
+    $(document).keyup(function (e) {
+        if (e.keyCode == 16) {
+            $("#keyboard-lower-container").show();
+            $("#keyboard-upper-container").hide();
+        }; 
+    });
+
 })
