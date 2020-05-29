937f574c4988c0c4005b1cb0a2ef25cbee1bae7d
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-29 00:53:20 -0500

    added senteces

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 $(document).ready(function () {
-    
-    $("#keyboard-upper-container").hide();
+  $("#keyboard-upper-container").hide();
 
-    $(document).keydown(function (e) {
-        if (e.keyCode === 16) {
-            $("#keyboard-lower-container").hide();
-            $("#keyboard-upper-container").show();
-        }; 
-    });
-    $(document).keyup(function (e) {
-        $(".highlight").removeClass("highlight");
-        if (e.keyCode === 16) {
-            $("#keyboard-lower-container").show();
-            $("#keyboard-upper-container").hide();
-        }; 
-    });
+  $(document).keydown(function (e) {
+    if (e.keyCode === 16) {
+      $("#keyboard-lower-container").hide();
+      $("#keyboard-upper-container").show();
+    }
+  });
+  $(document).keyup(function (e) {
+    $(".highlight").removeClass("highlight");
+    if (e.keyCode === 16) {
+      $("#keyboard-lower-container").show();
+      $("#keyboard-upper-container").hide();
+    }
+  });
 
-    $(document).keypress(function (e) {
-        $(`#${e.keyCode}`).addClass("highlight");
-    });
-})
+  $(document).keypress(function (e) {
+    $(`#${e.keyCode}`).addClass("highlight");
+  });
+
+  let sentences = [
+    "ten ate neite ate nee enet ite ate inet ent eate",
+    "Too ato too nOt enot one totA not anot tOO aNot",
+    "oat itain oat tain nate eate tea anne inant nean",
+    "itant eate anot eat nato inate eat anot tain eat",
+    "nee ene ate ite tent tiet ent ine ene ete ene ate",
+  ];
+  let sentenceIndex = 0;
+
+  $("#sentence").append(sentences[sentenceIndex]);
+  sentenceIndex++;
+});
