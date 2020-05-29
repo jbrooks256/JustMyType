016fae41734aa5b69de1c5ca083b606fd1598589
Justin Brooks <justinbrooks@Justins-MacBook-Pro.local>
2020-05-28 19:43:26 -0500

    added key highlights

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ $(document).ready(function () {
     $("#keyboard-upper-container").hide();
 
     $(document).keydown(function (e) {
-        if (e.keyCode = 16) {
+        if (e.keyCode === 16) {
             $("#keyboard-lower-container").hide();
             $("#keyboard-upper-container").show();
         }; 
     });
-    
     $(document).keyup(function (e) {
-        if (e.keyCode == 16) {
+        $(".highlight").removeClass("highlight");
+        if (e.keyCode === 16) {
             $("#keyboard-lower-container").show();
             $("#keyboard-upper-container").hide();
         }; 
     });
 
+    $(document).keypress(function (e) {
+        $(`#${e.keyCode}`).addClass("highlight");
+    });
 })
